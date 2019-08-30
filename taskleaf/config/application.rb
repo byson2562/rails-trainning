97ce7238480943069e08e08ceb1d12a72c3caa71
byson2562 <takumi.nakamura.by@gmail.com>
2019-08-30 17:12:42 +0900

    [fix] 日本語化辞書修正

--- taskleaf/config/application.rb
+++ taskleaf/config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -15,5 +14,9 @@ module Taskleaf
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # 日本語化設定
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
   end
 end
